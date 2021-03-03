f444142364091c65fcfd7d012d56a2bb532811ac
krasniukk <tania.krasniuk@gmail.com>
2021-03-03 22:45:32 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Notebook',
+   version='1.0',
+   description='Displays a notebook',
+   author='Krasniukk',
+   author_email='tetiana.krasniuk@ucu.edu.ua',
+   packages=['Notebook'],
+   install_requires=''
+)
